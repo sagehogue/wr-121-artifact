9c22711cdcafb5934b3fd073be12ec74cd73fd91
Sage Hogue <sagehogue@gmail.com>
2022-11-27 19:16:45 -0800

    removed dead code

--- src/Cube/Cube.jsx
+++ src/Cube/Cube.jsx
@@ -52,92 +52,41 @@ let CubeSpace = styled(Base)`
 // function createCSS
 
 let Face = styled(Base)`
-    margin: calc(-0.5 * ${cubeEdge});
-    width: ${cubeEdge};
-    height: ${cubeEdge};
-    // Dimensions of cube faces
+  margin: calc(-0.5 * ${cubeEdge});
+  // Keeps it centered on screen
+  width: ${cubeEdge};
+  height: ${cubeEdge};
+  // Dimensions of cube faces
 
-    box-shadow: inset 0 0 0 2px;
-    // Draw edges for visibility
+  box-shadow: inset 0 0 0 2px;
+  // Draw edges for visibility
 
-    --i: 0;
-    // default variables correspond to face 1, and are overridden for proceeding faces.
+  --i: 0;
+  // default variables correspond to face 1, and are overridden for proceeding faces.
 
+  &:nth-child(n + 5) {
+    --i: 1;
+  }
+  // overriding i and j values for faces 5 & 6.
 
-    // rotate3d(var(--i), calc(1 - var(--i)), 0, calc(var(--m, 0)*90deg)) translateZ(4em)
-    // Example transform
-
-    
-    
-
-    &:nth-child(n + 5) { --i: 1 }
-    // overriding i and j values for faces 5 & 6.
-  
-    ${() => {
-      let styles = "";
-      for (let i = 0; i < 6; i += 1) {
-        styles += `
+  ${() => {
+    let styles = "";
+    for (let i = 0; i < 6; i += 1) {
+      styles += `
       &:nth-child(${i + 1}) { --m: ${i < 4 ? i : Math.pow(-1, i)} }
      `;
-      }
-      styles += `
+    }
+    styles += `
   &:nth-child(n + 5) { --i: 1 }
   `;
-      return css`
-        ${styles}
-      `;
-    }}
-    // determines m value for each face
+    return css`
+      ${styles}
+    `;
+  }}
+  // determines m value for each face
 
     transform: rotate3d(var(--i), calc(1 - var(--i)), 0, calc(var(--m, 0)*90deg)) translateZ(calc(0.5 * ${cubeEdge}));
-    // position of each cube face
-
-
-
-  
-
-//   Another verbose version
-/*
-   &:nth-child(n + 5) { --i: 1; --j: 0 }
-  &:nth-child(2) { --m:  1 }
-  &:nth-child(3) { --m:  2 }
-  &:nth-child(4) { --m:  3 }
-  &:nth-child(5) { --m:  1 /*  1 = pow(-1, 4) */ }
-  &:nth-child(6) { --m: -1 /* -1 = pow(-1, 5) */ }
-*/
-
-//   Verbose version
-  /*
-  &:nth-child(1) {
-    background-color: blue;
-    transform: rotate3d(0 /* i */, 1 /* j */, 0 /* k */, 0deg /*  0*90° */)
-      translateZ(0.5 * ${cubeEdge});
-  }
-  &:nth-child(2) {
-    background-color: red;
-    transform: rotate3d(0 /* i */, 1 /* j */, 0 /* k */, 90deg /*  1*90° */)
-      translateZ(0.5 * ${cubeEdge});
-  }
-  &:nth-child(3) {
-    background-color: green;
-    transform: rotate3d(0 /* i */, 1 /* j */, 0 /* k */, 180deg /*  2*90° */)
-      translateZ(0.5 * ${cubeEdge});
-  }
-  &:nth-child(4) {
-    background-color: orange;
-    transform: rotate3d(0 /* i */, 1 /* j */, 0 /* k */, 270deg /*  3*90° */)
-      translateZ(0.5 * ${cubeEdge});
-  }
-  &:nth-child(5) {
-    background-color: yellow;
-    transform: rotate3d(1 /* i */, 0 /* j */, 0 /* k */, 90deg /*  1*90° */)
-      translateZ(0.5 * ${cubeEdge});
-  }
-  &:nth-child(6) {
-    background-color: purple;
-    transform: rotate3d(1 /* i */, 0 /* j */, 0 /* k */, -90deg /* -1*90° */)
-      translateZ(0.5 * ${cubeEdge});
-  } */
+  // position of each cube face
 `;
 
 export default function Cube() {
